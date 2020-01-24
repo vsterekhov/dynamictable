9804d37b17279d8308654b1422083476c8b23c26
vsterekhov <vsterekhov88@gmail.com>
2020-01-25 00:50:07 +0300

    ng g c component/dynamic-table --skipTests=true

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutComponent } from './component/main-layout/main-layout.component';
+import { DynamicTableComponent } from './component/dynamic-table/dynamic-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    DynamicTableComponent
   ],
   imports: [
     BrowserModule,
